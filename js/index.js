5b6957c5e98e96952454828ceb2e7efbdf6e9d73
ngocne <ngocdoojoon1999@gmail.com>
2021-08-10 20:54:27 +0700

    upload business

--- js/index.js
+++ js/index.js
@@ -1,9 +1,21 @@
-$(function(){
+$(function () {
     var $toggler = $('.header__navbar-toggler'),
-        $dropdown = $('.menu__dropdown');
-        $toggler.click(function(){
-            $(this).toggleClass('active');
-            $dropdown.toggleClass('active');
-            return false;
-        });
+        $dropdown = $('#menu__dropdown'),
+        $imgToggler = $('.menu-toggler'),
+        $close = $('.menu-close');
+    $toggler.click(function () {
+        $(this).toggleClass('is-active');
+        $imgToggler.toggleClass('is-disabled');
+        $close.toggleClass('is-active');
+        $dropdown.toggleClass('is-active');
+        return false;
+    });
+    $('body').on('click', function (e) {
+        if (!$dropdown.is(e.target) 
+            && $dropdown.has(e.target).length === 0 
+            && $('.is-active').has(e.target).length === 0
+        ) {
+            $toggler.removeClass('is-active');  
+        }
+    }); 
 });
